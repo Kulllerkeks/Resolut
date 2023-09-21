8d9a65c9bcbca9130fba0219604093a65db267db
kulllerkeks <kulllerkeks@gmail.com>
2023-09-22 00:04:16 +0200

    added module and readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Resolut
-A Lua module for easy resolution scaling
+A simple Lua module for easy resolution scaling.
+
+
+## Usage
+
+To use Resolut, put it into your work folder and require it in your code
+
+The constructor needs the original width and height of the application to calculate the ratio based on the changes and reserve the original ratio.
+You can pass in the Gamera cam too to set the window dimensions and center the camera on the screen.
+```lua
+Resolut = require("resolut").new(800,600)
+```
+
+after that you need to call the resize function in love.resize
+
+```lua
+funcion love.resize(w,h)
+    Resolut:Resize()
+end
+```
+and you are good to go. Enjoy the scaling!
---
+++ resolut.lua
@@ -0,0 +1,54 @@
+--A module to scale the screenresolution with Gamera automatically 
+--while preserving the original aspect ratio
+require("love")
+
+local resolut = {
+  window_width,
+  window_height,
+  original_width,
+  original_height,
+  ratio,
+  screenOffsetX,
+  screenOffsetY,
+  gameraCam,
+  debug = false
+}
+resolut.__index = resolut
+
+function resolut.new(original_width, original_height, gameraCam)
+  local self = {}
+  setmetatable(self, resolut)
+  self.original_width = original_width
+  self.original_height = original_height
+  self.gameraCam = gameraCam
+  return self
+end
+
+function resolut:Resize()
+  self.window_width, self.window_height = love.graphics.getDimensions()
+  self.ratio = math.min(self.window_width/self.original_width, self.window_height/self.original_height)
+  love.graphics.scale(self.ratio, self.ratio)
+  --center camera window on screen
+  local diffx = self.window_width - (self.original_width*self.ratio)
+  local diffy = self.window_height - (self.original_height*self.ratio)
+  if diffx ~= 0 then
+    diffx = diffx/2
+  end
+  if diffy ~= 0 then
+    diffy = diffy/2
+  end
+  self.screenOffsetX = diffx
+  self.screenOffsetY = diffy
+  if self.gameraCam then
+    self.gameraCam:setWindow(diffx,diffy,self.original_width*self.ratio,self.original_height*self.ratio)
+  end
+  
+  if debug then
+    print("window_width = " .. tostring(self.window_width))
+    print("window_height = " .. tostring(self.window_height))
+    print("screenOffsetX = " .. tostring(self.screenOffsetX))
+    print("screenOffsetY = " .. tostring(self.screenOffsetY))
+  end
+end
+
+return resolut
